723505e4905e4e65717a2c88c3efa339bc2403d5
Yasin Aktas <dev.yasin.aktas@gmail.com>
2025-04-27 14:58:07 +0300

    Odev 1

---
+++ Odev1/Odev1.kt
@@ -0,0 +1,58 @@
+package com.yapss.ders1.odev1
+
+class Odev1 {
+    // 1. grup
+    val sehir = "İstanbul"
+    val ulke = "Türkiye"
+    val telefon = "+90 532 123 45 67"
+    val postaKodu = "34000"
+    val email = "ornek@example.com"
+    val meslek = "Mühendis"
+    val stokMiktari = 150
+    val musteriAdi = "Ahmet Yılmaz"
+    val bakiye = 1250.50
+    val dogumGunu = "1995-06-21"
+    val maas = 18000.0
+    val medeniDurum = "Bekar"
+    val urunYorum = "Ürün çok kaliteli!"
+
+    // 2. grup
+    val odemeTarihi = "2025-04-27"
+    val odeme = 299.99
+    val siparisAdeti = 3
+    val arabaModeli = "Toyota Corolla"
+    val kitapAdi = "Suç ve Ceza"
+    val yayinlamaTarihi = "2023-09-15"
+    val indirimMiktari = 15.0
+    val odaSayisi = 2
+    val enlem = 41.0082
+    val boylam = 28.9784
+    val urunAdi = "Laptop"
+    val yemekFiyati = 75.50
+
+    // 3. grup
+    val marka = "Samsung"
+    val muzikAdi = "Imagine - John Lennon"
+    val videoSuresi = "02:35"
+    val urunPuani = 4.8
+    val resimAdi = "manzara.png"
+    val dosyaFormati = "JPEG"
+    val renk = "Mavi"
+    val renkKodu = "#0000FF"
+    val telefonModeli = "iPhone 14"
+    val ekranBoyutu = 6.7
+    val agirlik = 180.0 // gram
+
+    // 4. grup
+    val ulusalGun = "29 Ekim Cumhuriyet Bayramı"
+    val tatilGunu = "Yılbaşı"
+    val rezervasyonTarihi = "2025-05-10"
+    val sokakAdi = "Atatürk Caddesi"
+    val otobusHatti = "500T"
+    val kalanDakika = 12
+    val takipKodu = "TR1234567890"
+    val kuponSuresi = 30 // gün
+    val kuponKodu = "YAZINDIRIM"
+    val faturaAdresi = "İstanbul, Beşiktaş, Barbaros Bulvarı No:15"
+
+}
---
+++ Odev1/Odev1Main.kt
@@ -0,0 +1,69 @@
+package com.yapss.ders1.odev1
+
+fun main() {
+    val odev = Odev1()
+
+    // 1. Grup
+    println("=== 1. Grup ===")
+    println("Şehir: ${odev.sehir}")
+    println("Ülke: ${odev.ulke}")
+    println("Telefon: ${odev.telefon}")
+    println("Posta Kodu: ${odev.postaKodu}")
+    println("Email: ${odev.email}")
+    println("Meslek: ${odev.meslek}")
+    println("Stok Miktarı: ${odev.stokMiktari}")
+    println("Müşteri Adı: ${odev.musteriAdi}")
+    println("Bakiye: ${odev.bakiye}")
+    println("Doğum Günü: ${odev.dogumGunu}")
+    println("Maaş: ${odev.maas}")
+    println("Medeni Durum: ${odev.medeniDurum}")
+    println("Ürün Yorum: ${odev.urunYorum}")
+
+    println()
+
+    // 2. Grup
+    println("=== 2. Grup ===")
+    println("Ödeme Tarihi: ${odev.odemeTarihi}")
+    println("Ödeme: ${odev.odeme}")
+    println("Sipariş Adeti: ${odev.siparisAdeti}")
+    println("Araba Modeli: ${odev.arabaModeli}")
+    println("Kitap Adı: ${odev.kitapAdi}")
+    println("Yayınlama Tarihi: ${odev.yayinlamaTarihi}")
+    println("İndirim Miktarı: ${odev.indirimMiktari}")
+    println("Oda Sayısı: ${odev.odaSayisi}")
+    println("Enlem: ${odev.enlem}")
+    println("Boylam: ${odev.boylam}")
+    println("Ürün Adı: ${odev.urunAdi}")
+    println("Yemek Fiyatı: ${odev.yemekFiyati}")
+
+    println()
+
+    // 3. Grup
+    println("=== 3. Grup ===")
+    println("Marka: ${odev.marka}")
+    println("Müzik Adı: ${odev.muzikAdi}")
+    println("Video Süresi: ${odev.videoSuresi}")
+    println("Ürün Puanı: ${odev.urunPuani}")
+    println("Resim Adı: ${odev.resimAdi}")
+    println("Dosya Formatı: ${odev.dosyaFormati}")
+    println("Renk: ${odev.renk}")
+    println("Renk Kodu: ${odev.renkKodu}")
+    println("Telefon Modeli: ${odev.telefonModeli}")
+    println("Ekran Boyutu: ${odev.ekranBoyutu}")
+    println("Ağırlık: ${odev.agirlik}")
+
+    println()
+
+    // 4. Grup
+    println("=== 4. Grup ===")
+    println("Ulusal Gün: ${odev.ulusalGun}")
+    println("Tatil Günü: ${odev.tatilGunu}")
+    println("Rezervasyon Tarihi: ${odev.rezervasyonTarihi}")
+    println("Sokak Adı: ${odev.sokakAdi}")
+    println("Otobüs Hattı: ${odev.otobusHatti}")
+    println("Kalan Dakika: ${odev.kalanDakika}")
+    println("Takip Kodu: ${odev.takipKodu}")
+    println("Kupon Süresi: ${odev.kuponSuresi}")
+    println("Kupon Kodu: ${odev.kuponKodu}")
+    println("Fatura Adresi: ${odev.faturaAdresi}")
+}
